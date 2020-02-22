5df81e06eed4086d7151f98ad911d786728cb103
Tyler Kennedy <tkennedy118@gmail.com>
2020-02-22 13:28:01 -0600

    change invalid checkbox class name to more meaningful name

--- script.js
+++ script.js
@@ -12,8 +12,6 @@ var passwordLengthEl = document.querySelector("#passwordLength");
 var passwordLength = passwordLengthEl.firstElementChild.textContent;
 
 
-
-
 /* function to display generated password into textarea */
 function writePassword() {
     
@@ -70,10 +68,20 @@ function generatePassword() {
             criteriaMet = true;
         } 
     }
+
+    console.log("before function");
     
     // exit functoin if no checkboxes are checked
     if (criteriaMet === false) {
+        
+        console.log("inside false");
+        // run function to make user aware
+        displayValidity(criteriaMet);
+
         return false;
+    } else {
+        console.log("inside true");
+        displayValidity(criteriaMet);
     }
     
     // generate the password
@@ -96,12 +104,22 @@ function setPasswordLength(event) {
     passwordLength = length;
 }
 
+// function to display if input was not valid
+function displayValidity(criteriaMet) {
+
+    // the div element containing invalid content, only to be dispayed if input is invalid
+    var invalidTextEl = document.querySelector(".checkbox-invalid");
+
+    // show if criteria not met, otherwise hide
+    if (criteriaMet === false) {
+        invalidTextEl.classList.remove("d-none");
+    } else {
+        invalidTextEl.classList.add("d-none");
+    }
+}
+
 // add event listener to generate button
 generateBtnEl.addEventListener("click", writePassword);
 
 // add event listener to password length options
 passwordLengthEl.addEventListener("change", setPasswordLength);
-
-
-
-
