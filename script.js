499b3892fb5224fe12cfeed0bc53fc6917255529
Tyler Kennedy <tkennedy118@gmail.com>
2020-02-22 01:25:27 -0600

    create function to generate password

--- script.js
+++ script.js
@@ -1,15 +1,84 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generateBtn");
+// store html button element in variable
+var generateBtnEl = document.querySelector("#generateBtn");
 
-// Write password to the #password input
+// store html checkbox elements in variable
+var checkboxesEl = document.querySelectorAll(".form-check-input");
+
+// store html password length element in variable
+var passwordLengthEl = document.querySelector("#inputNumChars");
+
+// store html textarea in variable
+var passwordOutEl = document.querySelector("passwordOut");
+
+/* function to display generated password into textarea */
 function writePassword() {
-    // var password = generatePassword();
-    var password = "Hello";
+    
+    // generate password
+    var password = generatePassword();
 
     var passwordText = document.querySelector("#passwordOut");
 
     passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+/* function to generate random password based on user-selected criteria */
+function generatePassword() {
+
+    /* LOCAL VARIABLES */
+    var password = "";
+    var criteriaMet = false;
+    
+    // nested array of possible characters
+    var possibleChars = 
+        [
+            ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"],
+            ["abcdefghijklmnopqrstuvwxyz"],
+            ["0123456789"],
+            ["!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"]
+        ];     
+
+    // will contain all chosen characters
+    var chosenChars = "";      
+
+    // get password length
+    var passwordLength = 9;
+
+    while (!criteriaMet) {
+
+        // fill chosenChars array with selected character values
+        for (var i = 0; i < possibleChars.length; i++) {
+
+            // true if checkbox value is checked
+            var isChecked = checkboxesEl[i].checked;
+            
+            if (isChecked) {
+
+                for (var j = 0; j < possibleChars[i].length; j++) {
+                    chosenChars += possibleChars[i][j];
+                }
+
+                criteriaMet = true;
+            } 
+        }
+        // if criteria not met, display it in the bootstrap css   
+    }
+
+    // generate the password
+    for (var i = 0; i < passwordLength; i++) {
+
+        var randomInt = Math.floor(Math.random() * chosenChars.length);
+        var randomChar = chosenChars[randomInt];
+
+        password += randomChar;
+    }
+
+    return password;
+}
+
+// add event listener to generate button
+generateBtnEl.addEventListener("click", writePassword);
+
+// add event listener to password length options
+// passwordLengthEl.addEventListener("change", getPasswordLength(event));
+
+
